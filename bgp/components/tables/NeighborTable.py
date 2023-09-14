d38337190222b026098dcf94ed81d1655d0ba588
Uteeaami <eetu.pienimaa@gmail.com>
2023-09-14 11:25:03 +0300

    Added some explanation to routing tables

--- bgp/components/tables/NeighborTable.py
+++ bgp/components/tables/NeighborTable.py
@@ -1,2 +1,9 @@
 #https://www.bgp.us/routing-table/
 #Adj-RIBs-Out?
+"""
+The Adj-RIBs-Out stores information the local BGP
+         speaker selected for advertisement to its peers.  The routing
+         information stored in the Adj-RIBs-Out will be carried in the
+         local BGP speaker's UPDATE messages and advertised to its
+         peers.
+"""
