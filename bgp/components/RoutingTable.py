a72b1a51b036955e4f5d67324f89f3f71e967e0e
Uteeaami <eetu.pienimaa@gmail.com>
2023-10-18 18:06:00 +0300

    Initial routingtable done

--- bgp/components/RoutingTable.py
+++ bgp/components/RoutingTable.py
@@ -4,6 +4,8 @@ class RoutingTable:
         self.routing_table = {}
 
     def add_connection(self, router_id, router_ip, destination_ip):
+        if router_id not in self.routing_table:
+            self.routing_table[router_id] = []
         self.routing_table[router_id].append((router_ip, destination_ip))
 
     def get_connections(self, router_id):
