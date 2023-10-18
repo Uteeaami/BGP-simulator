36a10105d1aada5b4ab4acffe8e310f9f7503513
Eino Korte <e.i.korte@gmail.com>
2023-10-18 17:35:25 +0300

    added scripts

---
+++ scripts/create-interfaces.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+start=0
+end=20
+
+for ((i=start; i<=end; i++)); do
+    tunctl
+    ip=($i + 1)
+    tap=$i
+    ip_address="10.0.5.$ip"
+    network_interface="tap$tap"
+    sudo ip addr add "$ip_address" dev "$network_interface"
+    echo "Added IP $ip_address to $network_interface"
+done
+
