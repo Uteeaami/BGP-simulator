91efb6a1e0118ea6c2c92fb0a59c449d12063b46
Uteeaami <eetu.pienimaa@gmail.com>
2023-10-19 00:02:34 +0300

    removed redundant files again

--- bgp/components/Interface.py
+++
@@ -1,9 +0,0 @@
-
-class Interface:
-    def __init__(self, name, ip_address, autonomous_system):
-        self.name = name
-        self.ip_address = ip_address
-        self.autonomous_system = autonomous_system
-        
-    def __str__(self):
-        return f"Name: {self.name}\n IP Address: {self.ip_address}\n Autonomous System: {self.autonomous_system}"
